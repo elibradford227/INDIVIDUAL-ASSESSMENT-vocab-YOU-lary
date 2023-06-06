d5396a6060ebdfae548887f1387f7c58b6e0a047
Eli Bradford <elibradford227@gmail.com>
2023-06-05 20:35:02 -0500

    added ability to update entry

--- api/entryData.js
+++ api/entryData.js
@@ -58,6 +58,18 @@ const deleteEntry = (firebaseKey) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const getSingleEntry = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/entries/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
 export {
-  getEntries, createEntry, updateEntry, deleteEntry
+  getEntries, createEntry, updateEntry, deleteEntry, getSingleEntry
 };
--- events/domEvents.js
+++ events/domEvents.js
@@ -1,9 +1,10 @@
-import { getEntries, deleteEntry } from '../api/entryData';
+import { getEntries, deleteEntry, getSingleEntry } from '../api/entryData';
+import addEntry from '../forms/addEntry';
 import { cardsOnDom } from '../pages/entries';
 
 const domEvents = () => {
   document.querySelector('#main').addEventListener('click', (e) => {
-    if (e.target.id.includes('delete-book')) {
+    if (e.target.id.includes('delete-entry')) {
       // eslint-disable-next-line no-alert
       if (window.confirm('Want to delete?')) {
         const [, firebaseKey] = (e.target.id.split('--'));
@@ -11,6 +12,11 @@ const domEvents = () => {
           getEntries().then(cardsOnDom);
         });
       }
+    }
+    if (e.target.id.includes('edit-entry-btn')) {
+      const [, firebaseKey] = e.target.id.split('--');
+
+      getSingleEntry(firebaseKey).then((entryObj) => addEntry(entryObj));
     }
   });
 };
